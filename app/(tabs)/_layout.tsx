f070b112aa6bccb08904a7a4987d4933c46ac326
abr-ya <gamer05@list.ru>
2026-03-06 23:59:25 +0300

    feat: Add tabs navigation

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,33 @@
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: true,
+        tabBarStyle: { display: "flex" },
+        tabBarActiveTintColor: "#0066cc",
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
